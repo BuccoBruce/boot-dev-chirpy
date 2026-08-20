571b6b0fe8df85dceb37803423cc0d1e5057d455
buccobruce <dcmcmillan@gmail.com>
2026-08-19 22:33:45 -0700

    Added JSON filtering

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -43,6 +45,48 @@ func (cfg *apiConfig) resetHits() http.Handler {
 	})
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	payload := map[string]string{
+		"error": msg,
+	}
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+
+	if err != nil {
+		log.Printf("Error marshalling JSON %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func filterChirp(chirp string) string {
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(chirp, " ")
+	for i, word := range words {
+		for _, badWord := range badWords {
+			if strings.ToLower(word) == strings.ToLower(badWord) {
+				words[i] = "****"
+			}
+		}
+	}
+	censoredChirp := strings.Join(words, " ")
+	return censoredChirp
+}
+
 func main() {
 
 	apiCfg := apiConfig{}
@@ -53,9 +97,37 @@ func main() {
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	}
+
+	chirp := func(w http.ResponseWriter, r *http.Request) {
+		type parameters struct {
+			Body string `json:"body"`
+		}
+		type cleanedParameters struct {
+			Body string `json:"cleaned_body"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		params := parameters{}
+		cleanedParams := cleanedParameters{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			respondWithError(w, 400, "Error decoding parameters")
+			return
+		}
+		if len(params.Body) > 140 {
+			respondWithError(w, 400, "Chirp is too long")
+			return
+		}
+
+		cleanedParams.Body = filterChirp(params.Body)
+		respondWithJson(w, 200, cleanedParams)
+	}
+
 	myHandler.Handle("GET /admin/metrics", apiCfg.getHits())
 	myHandler.HandleFunc("GET /api/healthz", rep)
 	myHandler.Handle("GET 	/admin/reset", apiCfg.resetHits())
+	myHandler.Handle("POST 	/admin/reset", apiCfg.resetHits())
+	myHandler.HandleFunc("POST /api/validate_chirp", chirp)
 
 	s := http.Server{
 		Addr:    ":8080",
